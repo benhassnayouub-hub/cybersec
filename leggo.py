0e2200f9e12f2fb674c65d4bbac8eb028fe3b699
Ayoub <benhassnayouub@gmail.com>
2026-02-02 10:25:51 +0100

    Refactor main function for file reading and error handling
    Fix indentation, variable names, and exception handling in the main function.

--- leggo.py
+++ leggo.py
@@ -1,1 +1,15 @@
+def main():
+    # import sys 
 
+  nomefile = "logfile.txt"
+  try:
+      f=open(nomefile, "r" , encoding='utf-8')
+      right=f.readlines()
+      for riga in  righe:
+          print(riga, end="")
+  expect FilenotFoundError as e:
+         print(f"[-] errore blocante:{str(e)}")
+
+if __name__=="__main__":
+     main() 
+  
